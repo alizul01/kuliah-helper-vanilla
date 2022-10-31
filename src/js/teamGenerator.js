bfcc186c224fb276095d570d0dbdf0a8f91ce6e3
Ali Zulfikar <alizulfikar032@gmail.com>
2022-10-31 16:28:10 +0700

    Fix bug

--- src/js/teamGenerator.js
+++ src/js/teamGenerator.js
@@ -1,51 +1,41 @@
 const generate = document.getElementById('generate');
-        const result = document.querySelector('.result');
-        const member = document.getElementById('member');
-        const team = document.getElementById('team');
+const result = document.querySelector('.result');
+const member = document.getElementById('member');
+const team = document.getElementById('team');
 
-        generate.addEventListener('click', () => {
-            if (member.value == '' || team.value == '') {
-                alert('Please fill all the field');
-            } else {
-                const teamName = team.value.split(',');
-                const teamMember = member.value;
+generate.addEventListener('click', () => {
+    if (member.value == '' || team.value == '') {
+        alert('Please fill all the field');
+    } else {
+        const teamName = team.value.split(',');
+        const teamMember = member.value;
 
-                if (teamName.length < teamMember) {
-                    alert('Team member is more than team name');
-                } else {
-                    const team = [];
-                    
-                    for (let i = 0; i < teamMember; i++) {
-                        team.push([]);
-                    }
+        if (teamName.length < teamMember) {
+            alert('Team member is more than team name');
+        } else {
+            const team = [];
 
-                    let i = 0;
-                    while (teamName.length > 0) {
-                        const rand = Math.floor(Math.random() * teamName.length);
-                        team[i++].push(teamName[rand]);
-                        teamName.splice(rand, 1);
-                        if (i == teamMember) {
-                            i = 0;
-                        }
-                    }
+            for (let i = 0; i < teamMember; i++) {
+                team.push([]);
+            }
 
-                    result.innerHTML = '';
-                    team.forEach((item, index) => {
-                        result.innerHTML += `<h2>Team ${index + 1}</h2>`;
-                        item.forEach((name) => {
-                            result.innerHTML += `<p>${name}</p>`;
-                        });
-                    })
+            let i = 0;
+            while (teamName.length > 0) {
+                const rand = Math.floor(Math.random() * teamName.length);
+                team[i++].push(teamName[rand]);
+                teamName.splice(rand, 1);
+                if (i == teamMember) {
+                    i = 0;
                 }
             }
-        });
 
-// make O(log n) power of n
-
-function power(base, exponent) {
-    if (exponent == 0) {
-        return 1;
-    } else {
-        return base * power(base, exponent - 1);
+            result.innerHTML = '';
+            team.forEach((item, index) => {
+                result.innerHTML += `<h2>Team ${index + 1}</h2>`;
+                item.forEach((name) => {
+                    result.innerHTML += `<p>${name}</p>`;
+                });
+            })
+        }
     }
-}
+});
